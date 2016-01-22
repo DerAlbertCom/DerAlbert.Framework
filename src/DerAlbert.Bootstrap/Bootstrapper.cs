4b51459fd682f6ac1fcb365e0893edb79c9f27b5
Albert Weinert <info@der-albert.com>
2016-01-22 19:08:12 +0100

    removed DerAlbert.Runner.MSpec because it now an own project

--- src/DerAlbert.Bootstrap/Bootstrapper.cs
+++ src/DerAlbert.Bootstrap/Bootstrapper.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using StructureMap;
-using StructureMap.Graph;
 
 namespace DerAlbert.Bootstrap
 {
--- src/DerAlbert.Common/Mappings/AutoMapperLinqProjection.cs
+++ src/DerAlbert.Common/Mappings/AutoMapperLinqProjection.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Linq;
-using System.Reflection;
 
 namespace DerAlbert.Mappings
 {
